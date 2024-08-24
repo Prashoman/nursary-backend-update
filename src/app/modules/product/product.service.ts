8aa32c681445b9c701b7747eab79962685ca8a5b
Prashoman <badhonchakrabarti100823>
2024-08-24 13:01:34 +0600

    pagination and sorting done

--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -1,69 +1,90 @@
 import { TProduct } from "./product.interface";
-import { Product } from "./product.model";  
+import { Product } from "./product.model";
 
- 
+const productCreateDB = async (payload: TProduct) => {
+  const result = await Product.create(payload);
+  return result;
+};
 
- const productCreateDB = async (payload: TProduct) => {
-    const result = await Product.create(payload);
+const productGetDB = async (
+  productId: string,
+  payload: Record<string, unknown>
+) => {
+  const baseQuery = { ...payload };
+  const deleteQuery = ["searchTerm", "sort", "page", "limit"];
+  deleteQuery.forEach((element) => {
+    delete baseQuery[element];
+  });
+  let searchQuery;
+  let lowPriceQuery = 0;
+  let highPriceQuery = 10000000;
+  if (baseQuery.lowPrice) {
+    lowPriceQuery = Number(baseQuery.lowPrice);
+  }
+  if (baseQuery.highPrice) {
+    highPriceQuery = Number(baseQuery.highPrice);
+  }
+  if (payload.lowPrice) {
+    lowPriceQuery = Number(payload.lowPrice);
+  }
+  if (payload.highPrice) {
+    highPriceQuery = Number(payload.highPrice);
+  }
+  if (productId) {
+    const result = await Product.findById(productId);
     return result;
- }
+  }
+  if (payload.searchTerm) {
+    searchQuery = payload.searchTerm;
+  }
 
- const productGetDB = async (productId:string,payload:Record<string,unknown>) => {
-   const baseQuery = {...payload};
-   const deleteQuery = ['searchTerm'];
-   deleteQuery.forEach(element => {
-      delete baseQuery[element];
-   });
-      let  searchQuery;
-      let lowPriceQuery = 0;	
-      let highPriceQuery = 10000000;
-      if(baseQuery.lowPrice){
-         lowPriceQuery = Number(baseQuery.lowPrice);
-      }
-      if(baseQuery.highPrice){
-         highPriceQuery = Number(baseQuery.highPrice);
-      }
-      if(payload.lowPrice){
-         lowPriceQuery = Number(payload.lowPrice);
-      }
-      if(payload.highPrice){
-         highPriceQuery = Number(payload.highPrice);
-      }
-    if (productId) {
-        const result = await Product.findById(productId);
-        return result;
-    }
-    if(payload.searchTerm){
-      searchQuery = payload.searchTerm;
-    }
-    const searchingProduct =  Product.find(
-      {
-         title: { $regex: new RegExp(searchQuery as string, 'i') }
-      }
-    );
+  let sortQuery = "-createdAt";
+  if (payload.sort) {
+    sortQuery = payload.sort as string;
+  }
 
-    const result = await searchingProduct.find(
-      {
-         $and: [ {price: {$gte: lowPriceQuery}}, {price: {$lte: highPriceQuery}} ] 
-      }
-    ).populate('categoryId');
-    return result;
- }
+  let limit = Number(payload.limit) || 2;
+  let page = Number(payload.page) || 1;
+  const skip = (page - 1) * limit;
 
- const productUpdateDB = async (productId: string, payload: Partial<TProduct>) => {
-    const result = await Product.findByIdAndUpdate(productId, payload, { new: true });
-    return result;
- }
+  const searchingProduct = Product.find({
+    title: { $regex: new RegExp(searchQuery as string, "i") },
+  });
 
- const productDeleteDB = async (productId: string) => {
-    const result = await Product.findByIdAndDelete(productId);
-    return result;
- }
+  const filterPriceProduct = searchingProduct.find({
+    $and: [
+      { price: { $gte: lowPriceQuery } },
+      { price: { $lte: highPriceQuery } },
+    ],
+  });
 
+  const sortProduct = filterPriceProduct.sort(sortQuery);
+
+  const result = await sortProduct
+    .skip(skip)
+    .limit(limit)
+    .populate("categoryId");
+  return result;
+};
+
+const productUpdateDB = async (
+  productId: string,
+  payload: Partial<TProduct>
+) => {
+  const result = await Product.findByIdAndUpdate(productId, payload, {
+    new: true,
+  });
+  return result;
+};
+
+const productDeleteDB = async (productId: string) => {
+  const result = await Product.findByIdAndDelete(productId);
+  return result;
+};
 
 export const ProductService = {
-    productCreateDB,
-    productGetDB,
-    productUpdateDB,
-    productDeleteDB
-}
+  productCreateDB,
+  productGetDB,
+  productUpdateDB,
+  productDeleteDB,
+};
